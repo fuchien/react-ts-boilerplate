5bc8fc7c36ff7efcf0897bdb8857ff85ec8ea1da
Chiien <fuchienhsu12@gmail.com>
2020-02-07 00:26:51 -0300

    feat: add duck Auth types

---
+++ src/store/ducks/Auth/types.ts
@@ -0,0 +1,30 @@
+/**
+ * Action types
+ */
+export enum AuthTypes {
+    SIGN_IN_REQUEST = 'authentication/SIGN_IN_REQUEST',
+    SIGN_UP_REQUEST = 'authentication/SIGN_UP_REQUEST',
+    SIGN_IN_SUCCESS = 'authentication/SIGN_IN_SUCCESS',
+    SIGN_IN_FAILURE = 'authentication/SIGN_IN_FAILURE',
+    SIGN_OUT = 'authentication/SIGN_OUT',
+}
+
+/**
+ * Data types
+ */
+export interface AuthUser {
+    fullName: string;
+    email: string;
+    employeeId: string;
+    sponsorId: string;
+    userId: string;
+}
+/**
+ * State type
+ */
+export interface AuthState {
+    readonly session: boolean;
+    readonly signed: boolean;
+    readonly loading: boolean;
+    readonly user: AuthUser;
+}
